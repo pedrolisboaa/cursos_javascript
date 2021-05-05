e6d24e0f6d8f661b8cddcd67983420e50cc5a4a0
Pedro Lisboa <phlisboa2000@gmail.com>
2021-05-04 23:49:36 -0300

    teste

--- Javascript e TypeScript - Luiz Otavio/1-basico/7-String/script.js
+++ Javascript e TypeScript - Luiz Otavio/1-basico/7-String/script.js
@@ -1,1 +1,9 @@
-const nomeUsuario = prompt('Digite seu nome:')
+// Escrevendo na tag selecionada da HTML
+
+let nome = prompt('Qual seu nome?')
+
+document.body.innerHTML += `Olá , ${nome}.<br>`
+document.body.innerHTML += `Seu nome tem ${nome.length} letras.<br>`
+document.body.innerHTML += `A segunda letra do seu nome è ${nome[1]}.<br>`
+document.body.innerHTML += `Seu nome com letra maiúscula ${nome.toUpperCase()}.<br>`
+document.body.innerHTML += `Seu nome com letra minuscula ${nome.toLowerCase()}.<br>`
---
+++ Javascript e TypeScript - Luiz Otavio/1-basico/9-array/index.js
@@ -0,0 +1,9 @@
+const alunos = ['Pedro', 'Juliana', 'Marcia']
+const novoAluno = 'Marcelo'
+const novaAluna = 'Pepita'
+
+alunos[3] = novoAluno
+alunos.push(novaAluna)
+
+console.log(alunos)
+console.log(alunos.length)
